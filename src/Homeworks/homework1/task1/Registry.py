1071028685ffc3e12c00496d47b88e856b6b3eda
Виктор Ханукаев <144259984+VITYANA@users.noreply.github.com>
2024-04-14 22:10:27 +0300

    Add task1

---
+++ src/Homeworks/homework1/task1/Registry.py
@@ -0,0 +1,49 @@
+import typing
+from collections import Counter, OrderedDict, defaultdict
+
+USER_TEXT = (
+    "Registry with ParentClass 'dict', default=dict and Elements:\n"
+    "\t1)Counter\n"
+    "\t2)OrderedDict\n"
+    "\t3)defaultdict\n"
+    "Enter class name: "
+)
+T = typing.TypeVar("T")
+
+
+class Registry(typing.Generic[T]):
+    def __init__(self, default: typing.Optional[typing.Type[T]] = None):
+        self.registry: dict[str, typing.Type[T]] = dict()
+        self.default: typing.Optional[typing.Type[T]] = default
+
+    def register(self, name: str) -> typing.Callable[[typing.Type[T]], typing.Type[T]]:
+        def _add(cls: typing.Type[T]) -> typing.Type[T]:
+            self.registry[name] = cls
+            return cls
+
+        if name in self.registry:
+            raise ValueError(f"Name {name} already in registry.")
+        return _add
+
+    def dispatch(self, name: str) -> ValueError | typing.Type[T]:
+        if name not in self.registry.keys():
+            if self.default is None:
+                raise ValueError(f"Registry haven't class named {name}.")
+            return self.default
+        return self.registry[name]
+
+
+if __name__ == "__main__":
+    try:
+        register = Registry[dict](default=dict)
+        register.register("Counter")(Counter)
+        register.register("OrderedDict")(OrderedDict)
+        register.register("defaultdict")(defaultdict)
+        print("Enter one name of bellowed classes:")
+        print("1)Counter")
+        print("2)OrderedDict")
+        print("3)defaultdict")
+        user_input = input()
+        print("Result:", register.dispatch(user_input))
+    except Exception:
+        print(f"No such class in registry.")
---
+++ src/Homeworks/homework1/task1/test.py
@@ -0,0 +1,67 @@
+from typing import Mapping
+
+import pytest
+
+from src.Homeworks.homework1.task1.Registry import *
+
+MAPPING_REGISTRY = Registry[Mapping]()
+MAPPING_REGISTRY_default = Registry[Mapping](default=dict)
+
+
+@MAPPING_REGISTRY.register(name="avl_tree")
+class AVL_tree(Mapping):
+    def __init__(self):
+        pass
+
+    def __getitem__(self, item):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+
+@MAPPING_REGISTRY_default.register(name="cartesian_tree")
+class Cartesian_tree(Mapping):
+    def __init__(self):
+        pass
+
+    def __getitem__(self, item):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+
+def test_register():
+    assert (
+        MAPPING_REGISTRY.registry["avl_tree"] == AVL_tree
+        and MAPPING_REGISTRY_default.registry["cartesian_tree"] == Cartesian_tree
+        and MAPPING_REGISTRY_default.default == dict
+    )
+
+
+def test_dispatch():
+    assert (
+        MAPPING_REGISTRY.dispatch("avl_tree") == AVL_tree
+        and MAPPING_REGISTRY_default.dispatch("cartesian_tree") == Cartesian_tree
+        and MAPPING_REGISTRY_default.dispatch("missed_name") == dict,
+    )
+
+
+def test_register_error():
+    with pytest.raises(ValueError):
+
+        @MAPPING_REGISTRY.register(name="avl_tree")
+        class DeadEnd:
+            pass
+
+
+def test_dispatch_error():
+    with pytest.raises(ValueError):
+        MAPPING_REGISTRY.dispatch("dropdown")
