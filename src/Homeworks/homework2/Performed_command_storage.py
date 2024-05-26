ec7d34a1d1b16a4d82087f7271b16143a4a38702
Виктор Ханукаев <144259984+VITYANA@users.noreply.github.com>
2024-05-26 18:50:57 +0300

    add task

---
+++ src/Homeworks/homework2/Performed_command_storage.py
@@ -0,0 +1,18 @@
+import src.Homeworks.homework2.Actions as actions
+
+
+class PerformedCommandStorage:
+    def __init__(self, numbers: list) -> None:
+        self.commands: list[actions.Action] = []
+        self.numbers: list = numbers
+
+    def apply(self, action: actions.Action) -> None:
+        action.do(self.numbers)
+        self.commands.append(action)
+
+    def cancel(self) -> None:
+        if len(self.commands) > 0:
+            action_to_del = self.commands.pop(-1)
+            action_to_del.undo(self.numbers)
+        else:
+            raise ValueError("Command storage is empty.")
---
+++ src/Homeworks/homework2/main.py
@@ -0,0 +1,50 @@
+import Actions
+from Performed_command_storage import PerformedCommandStorage
+
+import src.Homeworks.homework1.task1.Registry as Registry
+
+
+def write_info(act_registry: Registry.Registry) -> str:
+    result_str = "Available commands:\n" "1.info\n" "2.exit\n" "3.cancel\n"
+    for i, action in enumerate(act_registry.registry.keys()):
+        result_str += f"{i + 4}. {action}\n"
+    return result_str
+
+
+def main() -> None:
+    user_request = ""
+    user_collection = eval(input("input your collection([1, 2, 3], e.t.c): "))
+    info = write_info(Actions.ACTIONS_REGISTRY)
+    command_storage = PerformedCommandStorage(user_collection)
+    print(info)
+    while user_request != "exit":
+        user_request = input("Enter your request: ")
+        if user_request == "exit":
+            print("End programme")
+            break
+        elif user_request == "info":
+            print(info + f"Your collection: {user_collection}")
+        elif user_request == "cancel":
+            try:
+                command_storage.cancel()
+            except ValueError as error:
+                print(error)
+            else:
+                print("Your collection: ", user_collection)
+        else:
+            action, *arguments = user_request.split(" ")
+            arguments = map(int, arguments)
+            try:
+                command_storage.apply(Actions.ACTIONS_REGISTRY.dispatch(action)(*arguments))
+            except ValueError as error:
+                print(error)
+            except TypeError:
+                print("Wrong number of arguments")
+            except Exception as error:
+                print(f"Unexpected error: {error}")
+            finally:
+                print(f"Your collection: {user_collection}")
+
+
+if __name__ == "__main__":
+    main()
